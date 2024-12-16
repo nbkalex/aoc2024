a0ad4e3489f084c91a18218eae5ceec542b1be45
alex <alex.ioan.dragomir@gmail.com>
2024-12-17 00:34:49 +0200

    day15

--- day15/Program.cs
+++ day15/Program.cs
@@ -4,10 +4,38 @@ var input = File.ReadAllLines("in.txt").ToList();
 
 int emptyLineIndex = input.IndexOf("");
 var mapInput = input.Take(emptyLineIndex).ToArray();
-Dictionary<PointF, char> map = new Dictionary<PointF, char>();
+
+Point robot = Point.Empty;
+Dictionary<Point, char> map = new Dictionary<Point, char>();
 for (int i = 0; i < mapInput.Length; i++)
   for (int j = 0; j < mapInput[0].Length; j++)
-    map.Add(new PointF(j, i), mapInput[i][j]);
+  {
+    Point left = new Point(2 * j, i);
+    Point right = new Point(2 * j + 1, i);
+
+    char leftSymbol = ' ';
+    char rightSymbol = ' ';
+
+    if(mapInput[i][j] == '@')
+    {
+      leftSymbol = '@';
+      rightSymbol = '.';
+      robot = new Point(j, i);
+    }
+    else if (mapInput[i][j] == 'O')
+    {
+      leftSymbol = '[';
+      rightSymbol = ']';
+    }
+    else
+    {
+      leftSymbol = mapInput[i][j];
+      rightSymbol = mapInput[i][j];
+    }
+
+    map.Add(left, leftSymbol);
+    map.Add(right, rightSymbol);
+  }
 
 var directions = new Dictionary<char, Point>()
 {
@@ -17,7 +45,16 @@ var directions = new Dictionary<char, Point>()
   {'v', new Point(0, 1) },
 };
 
-PointF robot = map.First(kvp => kvp.Value == '@').Key;
+
+Console.Clear();
+foreach (var p in map)
+{
+  Console.SetCursorPosition(p.Key.X, p.Key.Y);
+  Console.Write(p.Value);
+}
+
+Console.ReadKey();
+
 
 var movements = input.Skip(emptyLineIndex + 1).ToArray();
 foreach (var m in movements)
@@ -25,40 +62,41 @@ foreach (var m in movements)
   foreach(char d in m)
   {
     var dir = directions[d];
-    List<PointF> points = new List<PointF>();
-    PointF current = robot;
+    List<(Point,char)> points = new List<(Point, char)>();
+    Point current = robot;
+    
     while(map[current] != '.' && map[current] != '#')
-    {
-      points.Add(current);
-      current = new PointF(current.X + dir.X, current.Y + dir.Y);
-    }
+      points.Add((current, map[current]));
 
-    if(map[current] == '.')
-    { 
-      map[robot] ='.';
-      if(points.Count == 1)
-        robot = current;
-      else
-      { 
-        robot = points[1];
-        map[current] = 'O';
+    if(points.Count > 2)
+    {
+      for(int i = 0; i < points.Count; i++)
+      {
+
       }
 
-      map[robot] = '@';
     }
 
+
+    if(points.Count > 1)
+    {
+      map[robot] = '.';
+      robot = points[1].Item1;
+      map[robot] = '@';
+    }    
+
     //Console.Clear();
-    //foreach(var p in map)
+    //foreach (var p in map)
     //{
     //  Console.SetCursorPosition(p.Key.X, p.Key.Y);
     //  Console.Write(p.Value);
-    //}  
+    //}
 
     //Console.ReadKey();
   }
 }
 
-var boxes = map.Where(kvp => kvp.Value == 'O').ToArray();
-Console.WriteLine(boxes.Sum(p => p.Key.X + (p.Key.Y*100)));
+//var boxes = map.Where(kvp => kvp.Value == 'O').ToArray();
+//Console.WriteLine(boxes.Sum(p => p.Key.X + (p.Key.Y*100)));
 
-Console.WriteLine();
+//Console.WriteLine();
---
+++ day16/Program.cs
@@ -0,0 +1,94 @@
+﻿using System.Drawing;
+
+string[] input = File.ReadAllLines("in.txt");
+
+HashSet<Point> walls = new HashSet<Point>();
+
+Point start = Point.Empty;
+Point end = Point.Empty;
+
+for (int i = 0; i < input.Length; i++)
+  for (int j = 0; j < input[i].Length; j++)
+  {
+    var p = new Point(j, i);
+    if (input[i][j] == 'S')
+      start = p;
+    else if (input[i][j] == 'E')
+      end = p;
+    else if (input[i][j] == '#')
+      walls.Add(p);
+  }
+
+Point[] directions =
+{
+  new Point(0, 1),
+  new Point(-1,0),
+  new Point(0,-1),
+  new Point(1, 0),
+};
+
+
+Stack<(Point, Point, int, HashSet<Point>)> toGo = new Stack<(Point, Point, int, HashSet<Point>)>();
+toGo.Push((start, new Point(1, 0), 0, new HashSet<Point>() { start }));
+
+HashSet<Point> bestPaths = null;
+int minCost = int.MaxValue;
+
+Dictionary<(Point, Point), int> minCosts = new Dictionary<(Point, Point), int>();
+
+while (toGo.Any())
+{
+  var current = toGo.Pop();
+  foreach (Point dir in directions)
+  {
+    Point next = new Point(current.Item1.X + dir.X, current.Item1.Y + dir.Y);
+
+    if (walls.Contains(next) || current.Item4.Contains(next))
+      continue;
+
+    int additionalCost = dir == current.Item2 ? 0 : 1000;
+    int cost = current.Item3 + additionalCost + 1;
+    if(cost > minCost)
+      continue;
+
+    HashSet<Point> trace = new HashSet<Point>(current.Item4);
+    trace.Add(next);
+
+    if (next == end)
+    {
+      if (cost < minCost)
+      {
+        minCost = cost;
+        bestPaths = trace;
+      }
+
+      if (cost == minCost)
+      {
+        foreach (var p in trace)
+          bestPaths.Add(p);
+      }
+
+      continue;
+    }
+
+    var minHash = (next, dir);
+
+    if (minCosts.ContainsKey(minHash))
+    {
+      if (minCosts[minHash] >= cost)
+      {
+        toGo.Push((next, dir, cost, trace));
+        minCosts[minHash] = cost;
+      }
+    }
+
+    else
+    {
+      toGo.Push((next, dir, cost, trace));
+      minCosts[minHash] = cost;
+    }
+  }
+}
+
+Console.WriteLine(minCost);
+Console.WriteLine(bestPaths.Count);
