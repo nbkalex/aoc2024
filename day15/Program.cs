1ea954ee4583ff237bdae0fec3c8ab7ba5e0f95a
Alex Dragomir <alex.dragomir@caphyon.com>
2024-12-20 18:12:05 +0200

    day15 part 2

--- day15/Program.cs
+++ day15/Program.cs
@@ -10,35 +10,35 @@ Dictionary<Point, char> map = new Dictionary<Point, char>();
 for (int i = 0; i < mapInput.Length; i++)
   for (int j = 0; j < mapInput[0].Length; j++)
   {
-    if (mapInput[i][j] == '@')
-      robot = new Point(j, i);
-
-    map.Add(new Point(j, i), mapInput[i][j]);
-    //Point left = new Point(2 * j, i);
-    //Point right = new Point(2 * j + 1, i);
-
-    //char leftSymbol = ' ';
-    //char rightSymbol = ' ';
-
     //if (mapInput[i][j] == '@')
-    //{
-    //  leftSymbol = '@';
-    //  rightSymbol = '.';
     //  robot = new Point(j, i);
-    //}
-    //else if (mapInput[i][j] == 'O')
-    //{
-    //  leftSymbol = '[';
-    //  rightSymbol = ']';
-    //}
-    //else
-    //{
-    //  leftSymbol = mapInput[i][j];
-    //  rightSymbol = mapInput[i][j];
-    //}
 
-    //map.Add(left, leftSymbol);
-    //map.Add(right, rightSymbol);
+    //map.Add(new Point(j, i), mapInput[i][j]);
+    Point left = new Point(2 * j, i);
+    Point right = new Point(2 * j + 1, i);
+
+    char leftSymbol = ' ';
+    char rightSymbol = ' ';
+
+    if (mapInput[i][j] == '@')
+    {
+      leftSymbol = '@';
+      rightSymbol = '.';
+      robot = left;
+    }
+    else if (mapInput[i][j] == 'O')
+    {
+      leftSymbol = '[';
+      rightSymbol = ']';
+    }
+    else
+    {
+      leftSymbol = mapInput[i][j];
+      rightSymbol = mapInput[i][j];
+    }
+
+    map.Add(left, leftSymbol);
+    map.Add(right, rightSymbol);
   }
 
 var directions = new Dictionary<char, Point>()
@@ -50,14 +50,14 @@ var directions = new Dictionary<char, Point>()
 };
 
 
-Console.Clear();
-foreach (var p in map)
-{
-  Console.SetCursorPosition(p.Key.X, p.Key.Y);
-  Console.Write(p.Value);
-}
+//Console.Clear();
+//foreach (var p in map)
+//{
+//  Console.SetCursorPosition(p.Key.X, p.Key.Y);
+//  Console.Write(p.Value);
+//}
 
-Console.ReadKey();
+//Console.ReadKey();
 
 
 var movements = input.Skip(emptyLineIndex + 1).ToArray();
@@ -66,42 +66,91 @@ foreach (var m in movements)
   foreach (char d in m)
   {
     var dir = directions[d];
-    Point current = robot;
-    List<(Point, char)> points = new List<(Point, char)>()
+    List<(Point, char)> points = new List<(Point, char)>();
+
+    Queue<(Point, char)> queue = new Queue<(Point, char)>();
+    queue.Enqueue((robot, '.'));
+
+    bool canMove = true;
+    while (queue.Count > 0)
     {
-      (robot, map[robot])
-    };
+      var current = queue.Dequeue();
+      points.Add((current.Item1, current.Item2));
 
-    //if (dir.Y != 0)
-    //{
-      while (map[current] != '.' && map[current] != '#')
+      if (map[current.Item1] == '.' && current.Item1 != robot)
+        continue;
+
+      Point next = AddPoints(current.Item1, dir);
+
+      if (map[next] == '#')
       {
-        current = AddPoints(current, dir);
-        points.Add((current, map[current]));
+        canMove = false;
+        break;
       }
 
-      if (map[current] == '.')
+      if (map[next] != '.')
       {
-        for (var i = 1; i < points.Count; i++)
-          map[points[i].Item1] = points[i - 1].Item2;
+        if (dir.Y == 0)
+          queue.Enqueue((next, map[current.Item1]));
+        else
+        {
+          Point left = Point.Empty;
+          Point right = Point.Empty;
 
-        map[robot] = '.';
-        robot = AddPoints(robot, dir);
+          char leftC = ' ';
+          char rightC = ' ';
+
+          if (map[next] == '[')
+          {
+            var r = new Point(current.Item1.X + 1, current.Item1.Y);
+
+            bool containsR = points.Any(p => p.Item1 == r);
+
+            leftC = map[current.Item1];
+            rightC = current.Item1 == robot || !containsR ? '.' : map[r];
+            left = next;
+            right = new Point(next.X + 1, next.Y);
+          }
+          else
+          {
+            var l = new Point(current.Item1.X - 1, current.Item1.Y);
+
+            bool containsL = points.Any(p => p.Item1 == l);
+
+            leftC = current.Item1 == robot | !containsL ? '.' : map[l];
+            rightC = map[current.Item1];
+
+            left = new Point(next.X - 1, next.Y);
+            right = next;
+          }
+
+          queue.Enqueue((left, leftC));
+          queue.Enqueue((right, rightC));
+        }
       }
-    //}
+      else
+        queue.Enqueue((next, map[current.Item1]));
+    }
 
+    if (canMove)
+    { 
+      foreach (var p in points)
+        map[p.Item1] = p.Item2;
+
+      robot = AddPoints(robot, dir);
+    }
+
+    //Console.ReadKey();
     //Console.Clear();
     //foreach (var p in map)
     //{
     //  Console.SetCursorPosition(p.Key.X, p.Key.Y);
     //  Console.Write(p.Value);
     //}
-
-    //Console.ReadKey();
   }
 }
 
-var boxes = map.Where(kvp => kvp.Value == 'O').ToArray();
+var boxes = map.Where(kvp => kvp.Value == '[' ).ToArray();
 Console.WriteLine(boxes.Sum(p => p.Key.X + (p.Key.Y * 100)));
 
 //Console.WriteLine();
