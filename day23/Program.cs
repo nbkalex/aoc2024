358ad946915667a7e1e1d4bcd2fb3dbc65454094
alex <alex.ioan.dragomir@gmail.com>
2024-12-25 23:34:36 +0200

    day23 part2

--- day23/Program.cs
+++ day23/Program.cs
@@ -1,4 +1,6 @@
-﻿var connections = File.ReadAllLines("in.txt").Select(l => l.Split("-")).ToArray();
+﻿using System.Collections.Generic;
+
+var connections = File.ReadAllLines("in.txt").Select(l => l.Split("-")).ToArray();
 
 Dictionary<string, HashSet<string>> pcsCon = new Dictionary<string, HashSet<string>>();
 
@@ -9,7 +11,7 @@ for (int i = 0; i < connections.Length; i++)
   string pc1 = connections[i][0];
   string pc2 = connections[i][1];
 
-  if(!pcsCon.ContainsKey(pc1))
+  if (!pcsCon.ContainsKey(pc1))
     pcsCon.Add(pc1, new HashSet<string>());
   if (!pcsCon.ContainsKey(pc2))
     pcsCon.Add(pc2, new HashSet<string>());
@@ -20,14 +22,14 @@ for (int i = 0; i < connections.Length; i++)
 
 
 HashSet<string> parties = new HashSet<string>();
-foreach(var c in pcsCon)
+foreach (var c in pcsCon)
 {
   List<(string, string)> pairs = new List<(string, string)>();
-  for(int i = 0; i < c.Value.Count; i++)
-    for (int j = i+1; j < c.Value.Count; j++)
+  for (int i = 0; i < c.Value.Count; i++)
+    for (int j = i + 1; j < c.Value.Count; j++)
     {
       if (pcsCon[c.Value.ElementAt(i)].Contains(c.Value.ElementAt(j)))
-      { 
+      {
         List<string> party = new List<string>()
         {
           c.Key, c.Value.ElementAt(i), c.Value.ElementAt(j)
@@ -40,29 +42,45 @@ foreach(var c in pcsCon)
 
 Console.WriteLine(parties.Count(p => p[0] == 't' || p[2] == 't' || p[4] == 't'));
 
-HashSet<string> largestParty = new HashSet<string>();
+List<string> largestParty = new List<string>();
+
+var maxCons = pcsCon.Max(pc => pc.Value.Count);
 
 foreach (var c in pcsCon)
 {
-  Queue<HashSet<string>> queue = new Queue<HashSet<string>>();
-  queue.Enqueue((new HashSet<string>() { c.Key }));
-  while(queue.Count > 0)
+  foreach (var pc in c.Value)
   {
-    var current = queue.Dequeue();
+    List<string> tocheck = new List<string>(c.Value);
+    tocheck.Remove(pc);
 
-    foreach (var pc in c.Value.Except(current))
+    bool all = true;
+
+    foreach (var pc2 in tocheck)
     {
-      if (current.Any(c => !pcsCon[c].Contains(pc)))
-        continue;
+      foreach (var pc3 in tocheck)
+      { 
+        if (pc2 == pc3)
+          continue;
 
-      HashSet<string> newList = new HashSet<string>(current);
-      newList.Add(pc);
+        if (!pcsCon[pc2].Contains(pc3))
+        {
+          all = false;
+          break;
+        }
+      }
 
-      if (largestParty.Count < newList.Count)
-        largestParty = newList;
-      queue.Enqueue(newList);
+      if(!all)
+        break;
+    }
+
+    if(all)
+    {
+      tocheck.Add(c.Key);
+      largestParty = tocheck;
     }
   }
 }
 
-Console.WriteLine();
+largestParty.Sort();
+
+Console.WriteLine(string.Join(",", largestParty));
